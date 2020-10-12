fa81150a091cdef47d17fd7a0cf7c873558bb1d1
Hiren Suthar <sutharh@pmc.local>
2020-10-12 23:00:04 +0530

    testng runner and testng xml added

---
+++ src/test/java/testNGRunner.java
@@ -0,0 +1,8 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = {"src/main/java/Features"},
+        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
+        glue = {"Tests"})
+public class testNGRunner extends AbstractTestNGCucumberTests {
+}
