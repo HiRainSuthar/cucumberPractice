14801bdb6aecaf79900980ae03417dbbf43e2036
Hiren Suthar <sutharh@pmc.local>
2020-10-14 22:56:48 +0530

    html reporting added

--- src/test/java/testNGRunner.java
+++ src/test/java/testNGRunner.java
@@ -2,7 +2,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/main/java/Features"},
-        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
+        plugin = {"json:target/cucumberReports/cucumber.json", "html:target/site/cucumber-pretty"},
         glue = {"Tests"})
 public class testNGRunner extends AbstractTestNGCucumberTests {
 }
