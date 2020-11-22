e73c20832832294053ec8436e0486148b3a374b5
unknown <hireynsuthar@yahoo.com>
2020-11-22 17:32:03 +0530

    support added for parallel execution using testng

--- src/main/java/TestRunner/testNGRunner.java
+++ src/main/java/TestRunner/testNGRunner.java
@@ -2,10 +2,16 @@ package TestRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(features = {"src/main/java/Features"},
         plugin = {"json:target/cucumberReports/cucumber.json", "html:target/site/cucumber-pretty"},
         glue = {"tests"})
 public class testNGRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
